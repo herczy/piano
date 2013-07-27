e27d699f9afa9f95453e7c82122b619ab948b082
Viktor Hercinger <herczy@balabit.hu>
2013-07-27 19:09:30 +0200

    removed unused abstract() function

--- piano/utils.py
+++ piano/utils.py
@@ -7,11 +7,3 @@ def copy_args(dest, src):
 
     if hasattr(src, '__annotations__'):
         dest.__annotations__ = src.__annotations__
-
-
-def abstract(func):
-    def __wrapper(*args, **kwargs):
-        raise NotImplementedError("%s not implemented" % func.__name__)
-
-    copy_args(__wrapper, func)
-    return __wrapper
