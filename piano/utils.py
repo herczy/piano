c15b572c6a4d5f61cfaa3fbeb76e89fdb53c8a83
Viktor Hercinger <herczy@balabit.hu>
2013-07-27 13:45:47 +0200

    copy_args should check existence of attributes

--- piano/utils.py
+++ piano/utils.py
@@ -1,7 +1,12 @@
 def copy_args(dest, src):
-    dest.__name__ = src.__name__
-    dest.__doc__ = src.__doc__
-    dest.__annotations__ = src.__annotations__
+    if hasattr(src, '__name__'):
+        dest.__name__ = src.__name__
+
+    if hasattr(src, '__doc__'):
+        dest.__doc__ = src.__doc__
+
+    if hasattr(src, '__annotations__'):
+        dest.__annotations__ = src.__annotations__
 
 
 def abstract(func):
